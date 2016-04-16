110a4ce8fa34b114c1daae191b6f237058d70185
authorbeard <msnolan@gmail.com>
2016-04-16 14:54:59 -0400

    All models specs besides cart passing

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,15 @@ class Item < ActiveRecord::Base
   has_many :carts, through: :line_items
   belongs_to :category
 
+  before_create :set_default_inventory
+
+
+  def self.available_items
+    where("inventory > 0")
+  end
+
+  def set_default_inventory
+    self.inventory ||= 0
+  end
+
 end
